1d876d4608734117359200025ceefb00d9b4c3ff
piyush <15842633+piyush5103@users.noreply.github.com>
2020-10-18 18:29:35 +0530

    timeline cleaned

--- src/pages/join.js
+++ src/pages/join.js
@@ -94,10 +94,6 @@ const RenderBody = ({ meta, timeline }) => (
 
 		<Timeline mode={"alternate"}>
 
-			<Timeline.Item dot={<AiTwotoneRightCircle style={{ fontSize: 16, color: "black" }} />}>
-				<TimelineTitle>breuiveuir</TimelineTitle>
-				<TimelineContent>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </TimelineContent>
-			</Timeline.Item>
 
 
 			{timeline.map((item, i) => (
