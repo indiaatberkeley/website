0c1dcd2cbdaa01fb7629a225bd42855d7efbf314
piyush5103 <15842633+piyush5103@users.noreply.github.com>
2021-03-01 15:55:45 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { StaticQuery, graphql } from "gatsby";
 import styled from "@emotion/styled";
+import dimensions from "styles/dimensions";
+import { StaticQuery, graphql } from "gatsby";
+import 'styles/fonts.scss';
 import { Global } from "@emotion/core";
 import globalStyles from 'styles/global';
 import typeStyles from 'styles/typography';
-import dimensions from "styles/dimensions";
-import Footer from "components/Footer";
 import Header from "components/Header";
-import 'styles/fonts.scss';
+import Footer from "components/Footer";
+import PropTypes from "prop-types";
 
 const LayoutContainer = styled.div`
     max-width: ${dimensions.maxwidthDesktop}px;
